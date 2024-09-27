71e1c43c2160312ca2a4b5b9edd018e450e8d893
Adityakr321 <kalyanaditya32@gmail.com>
2024-09-27 14:09:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -158,6 +158,7 @@ app.post('/contact', (req, res) => {
 
 
 
-app.listen(3000,function(req,res){
-    console.log("Server is running on port 3000");
-})
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server started on port " + port);
+});
